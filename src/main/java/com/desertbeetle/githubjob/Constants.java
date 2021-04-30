cbafb137673d453609ff5c10b4e416b904f4db3f
Leo Fu <leofu@protonmail.com>
2021-04-29 18:02:51 -0700

    holding all constants

---
+++ src/main/java/com/desertbeetle/githubjob/Constants.java
@@ -0,0 +1,11 @@
+package com.desertbeetle.githubjob;
+
+public class Constants {
+    public static final String[] LOCATIONS = {"Boston", "San Francisco", "Log Angeles", "Denver", "Boulder", "Chicago", "New York" ,"Raleigh"};
+    public static final String[] LANGUAGES = {"Java", "C#", "Python", "Swift", "Objective-C", "Ruby", "Kotlin", "Go", "C++", "Javascript"};
+    public static final String REST_URI = "https://jobs.github.com/positions";
+    public static final String UTF8_SPACE = "%20";
+    public static final String UTF8_SHARP = "%23";
+    public static final String UTF8_PLUS  = "%2B";
+
+}
